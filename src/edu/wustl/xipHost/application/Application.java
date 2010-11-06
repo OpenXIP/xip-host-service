754477c1179411df4019a5ccb99712ce694b267a
Krych, Jaroslaw <jarek.krych@gmail.com>
2010-11-06 21:55:31 +0000

    Extended Application class to implement AVTListener and TargetIteratorListener. Provided implementation for interface methods. Added logging.
    SVN-Revision: 562

--- src/edu/wustl/xipHost/application/Application.java
+++ src/edu/wustl/xipHost/application/Application.java
@@ -4,11 +4,33 @@
 package edu.wustl.xipHost.application;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import org.apache.log4j.Logger;
+import org.dcm4che2.data.Tag;
+import edu.wustl.xipHost.avt2ext.ADQueryTarget;
+import edu.wustl.xipHost.avt2ext.AVTListener;
+import edu.wustl.xipHost.avt2ext.AVTQuery;
+import edu.wustl.xipHost.avt2ext.AVTRetrieveEvent;
+import edu.wustl.xipHost.avt2ext.AVTSearchEvent;
 import edu.wustl.xipHost.avt2ext.iterator.IterationTarget;
+import edu.wustl.xipHost.avt2ext.iterator.IteratorElementEvent;
+import edu.wustl.xipHost.avt2ext.iterator.IteratorEvent;
+import edu.wustl.xipHost.avt2ext.iterator.TargetElement;
+import edu.wustl.xipHost.avt2ext.iterator.TargetIteratorListener;
+import edu.wustl.xipHost.avt2ext.iterator.TargetIteratorRunner;
+import edu.wustl.xipHost.dataModel.Patient;
+import edu.wustl.xipHost.dataModel.SearchResult;
+import edu.wustl.xipHost.hostControl.Util;
+import edu.wustl.xipHost.worklist.WorklistEntry;
 
-public class Application {	
-	
+public class Application implements AVTListener, TargetIteratorListener {	
+	final static Logger logger = Logger.getLogger(Application.class);
 	UUID id;
 	String name;
 	File exePath;
@@ -111,5 +133,90 @@ public class Application {
 
 	public void setIterationTarget(IterationTarget iterationTarget) {
 		this.iterationTarget = iterationTarget;
+	}
+	
+	File appTmpDir;
+	public void setApplicationTmpDir(File tmpDir){
+		appTmpDir = tmpDir;
+	}
+	public File getApplicationTmpDir() {
+		return appTmpDir;		
+	}
+	
+	File appOutputDir;
+	public void setApplicationOutputDir(File outDir){				
+		try {
+			appOutputDir = Util.create("xipOUT_" + getName() + "_", "", outDir);
+		} catch (IOException e) {
+			logger.error(e, e);
+		}		
+	}
+	public File getApplicationOutputDir() {		
+		return appOutputDir;
+	}
+	
+	public void launch(){
+		
+	}
+
+	AVTQuery avtQuery;
+	public void setWorklistEntry(WorklistEntry entry) {
+		String studyInstanceUID = entry.getStudyInstanceUID();
+		Map<Integer, Object> dicomCriteria = new HashMap<Integer, Object>();
+		Map<String, Object> aimCriteria = new HashMap<String, Object>();			
+		dicomCriteria.put(Tag.StudyInstanceUID, studyInstanceUID);
+		avtQuery = new AVTQuery(dicomCriteria, aimCriteria, ADQueryTarget.PATIENT, null, null);
+		avtQuery.addAVTListener(this);
+		Thread t = new Thread(avtQuery);
+		t.start();		
+	}
+	
+	SearchResult selectedDataSearchResult;
+	@Override
+	public void searchResultsAvailable(AVTSearchEvent e) {
+		SearchResult selectedDataSearchResult = (SearchResult)e.getSource();
+		logger.debug("Found patients: ");
+		for(Patient patient : selectedDataSearchResult.getPatients()){
+			logger.debug(patient.getPatientID() + " " + patient.getPatientName());
+		}
+		//Start TargetIteratorRunner
+		TargetIteratorRunner targetIter = new TargetIteratorRunner(selectedDataSearchResult, getIterationTarget(), avtQuery, getApplicationTmpDir(), this);
+		try {
+			Thread t = new Thread(targetIter);
+			t.start();
+		} catch(Exception e1) {
+			logger.error(e1, e1);
+		}
+	}
+
+	@Override
+	public void retriveResultsAvailable(AVTRetrieveEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void notifyException(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	Iterator<TargetElement> iter;
+	@SuppressWarnings("unchecked")
+	@Override
+	public void fullIteratorAvailable(IteratorEvent e) {
+		iter = (Iterator<TargetElement>)e.getSource();
+		logger.debug("Full TargetIterator available at time " + System.currentTimeMillis());
+	}
+
+	List<TargetElement> targetElements = new ArrayList<TargetElement>();
+	@Override
+	public void targetElementAvailable(IteratorElementEvent e) {
+		synchronized(targetElements){
+			TargetElement element = (TargetElement) e.getSource();
+			logger.debug("TargetElement available. ID: " + element.getId() + " at time " + System.currentTimeMillis());
+			targetElements.add(element);
+			targetElements.notify();
+		}
 	}
 }
