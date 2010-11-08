b7e397e6aafcc6f5f6521601a6cf85a2219c1ea8
Krych, Jaroslaw <jarek.krych@gmail.com>
2010-11-08 14:47:12 +0000

    Added internal Runnable() to addWorklistEntry() of WorklistImpl to perform process of adding worklist entry in a Thread and returning at the same time boolean value asynchronously to the client.
    SVN-Revision: 586

--- src/edu/wustl/xipHost/worklist/WorklistImpl.java
+++ src/edu/wustl/xipHost/worklist/WorklistImpl.java
@@ -25,47 +25,58 @@ import edu.wustl.xipHost.application.ApplicationManagerFactory;
 public class WorklistImpl implements Worklist {
 	final static Logger logger = Logger.getLogger(WorklistImpl.class);
 	ApplicationManager appMgr = ApplicationManagerFactory.getInstance();
+	
 	@Override
-	public boolean addWorklistEntry(WorklistEntry entry) {
-		String studyInstanceUID = entry.getStudyInstanceUID();
-		Application app = entry.getApplication();
+	public boolean addWorklistEntry(final WorklistEntry entry) {
+		final String studyInstanceUID = entry.getStudyInstanceUID();
 		if(studyInstanceUID == null || studyInstanceUID.isEmpty()){
 			logger.warn("Worklist recieved StudyInstanceUID: " + studyInstanceUID);
 			return false;
 		}
-		if(logger.isDebugEnabled()){
-			logger.debug("Worklist recieved StudyInstanceUID: " + studyInstanceUID);
-			if(app != null){
-				logger.debug("Worklist recieved application: ");
-				logger.debug("ID: " + app.getId());
-				logger.debug("Name: " + app.getName());
-				logger.debug("Type:" + app.getType());
-				logger.debug("Requires GUI: " + app.isRequiresGUI());
-				logger.debug("Iteration target: " + app.getIterationTarget().toString());
-				logger.debug("Allowable concurrent instances: " + app.getConcurrentInstances());
-				logger.debug("WG-23 data model type: " + app.getWg23DataModelType());
-			} else {
-				logger.debug("Worklist recieved application: " + app);
+		Runnable runner = new Runnable() {
+			@Override
+			public void run() {
+				Application app = entry.getApplication();
+				if(logger.isDebugEnabled()){
+					logger.debug("Worklist recieved StudyInstanceUID: " + studyInstanceUID);
+					if(app != null){
+						logger.debug("Worklist recieved application: ");
+						logger.debug("ID: " + app.getId());
+						logger.debug("Name: " + app.getName());
+						logger.debug("Type:" + app.getType());
+						logger.debug("Requires GUI: " + app.isRequiresGUI());
+						logger.debug("Iteration target: " + app.getIterationTarget().toString());
+						logger.debug("Allowable concurrent instances: " + app.getConcurrentInstances());
+						logger.debug("WG-23 data model type: " + app.getWg23DataModelType());
+					} else {
+						logger.debug("Worklist recieved application: " + app);
+					}
+				}
+				if(app != null && appMgr.hasApplication(app.getId()) == false){
+					appMgr.addApplication(app);
+				}
+				appMgr.setTmpDir(new File("C:/TmpXIP"));
+				appMgr.setOutputDir(new File("C:/OutXIP"));
+				File tmpDir = appMgr.getTmpDir();
+				File outDir = appMgr.getOutputDir();
+				app.setApplicationTmpDir(tmpDir);
+				app.setApplicationOutputDir(outDir);
+				URL hostServiceURL = null;
+				try {
+					hostServiceURL = new URL("http://localhost:8080/xiphostservice/host");
+				} catch (MalformedURLException e) {
+					logger.error(e, e);
+				}
+				URL appServiceURL = appMgr.generateNewApplicationServiceURL();
+				
+				
+				app.launch(hostServiceURL, appServiceURL);
+				app.setWorklistEntry(entry);
+				
 			}
-		}
-		if(app != null && appMgr.hasApplication(app.getId()) == false){
-			appMgr.addApplication(app);
-		}
-		appMgr.setTmpDir(new File("C:/TmpXIP"));
-		appMgr.setOutputDir(new File("C:/OutXIP"));
-		File tmpDir = appMgr.getTmpDir();
-		File outDir = appMgr.getOutputDir();
-		app.setApplicationTmpDir(tmpDir);
-		app.setApplicationOutputDir(outDir);
-		URL hostServiceURL = null;
-		try {
-			hostServiceURL = new URL("http://localhost:8080/xiphostservice/host");
-		} catch (MalformedURLException e) {
-			logger.error(e, e);
-		}
-		URL appServiceURL = appMgr.generateNewApplicationServiceURL();
-		app.launch(hostServiceURL, appServiceURL);
-		app.setWorklistEntry(entry);
+		};
+		Thread t = new Thread(runner);
+		t.start();		
 		return true;
 	}
 
