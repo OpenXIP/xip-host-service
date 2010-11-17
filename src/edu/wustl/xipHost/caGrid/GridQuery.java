8d872a45edc5ee6995f4a30e426cbc6a0ad805b1
Krych, Jaroslaw <jarek.krych@gmail.com>
2010-11-17 16:37:50 +0000

    Modified grid query to implement and use new generic interface and classes from dataAccess package. Refactored constructor to make provisions to make it compatibile with the TargetIterator.
    SVN-Revision: 639

--- src/edu/wustl/xipHost/caGrid/GridQuery.java
+++ src/edu/wustl/xipHost/caGrid/GridQuery.java
@@ -1,17 +1,24 @@
 /**
- * Copyright (c) 2008 Washington University in St. Louis. All Rights Reserved.
+ * Copyright (c) 2010 Washington University in St. Louis. All Rights Reserved.
  */
 package edu.wustl.xipHost.caGrid;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.ConnectException;
 import java.rmi.RemoteException;
+import java.util.Map;
 import org.apache.axis.types.URI.MalformedURIException;
 import org.apache.log4j.Logger;
 import org.globus.wsrf.encoding.ObjectSerializer;
 import org.globus.wsrf.encoding.SerializationException;
-
 import javax.xml.namespace.QName;
 import edu.wustl.xipHost.caGrid.GridLocation;
+import edu.wustl.xipHost.dataAccess.DataAccessListener;
+import edu.wustl.xipHost.dataAccess.Query;
+import edu.wustl.xipHost.dataAccess.QueryEvent;
+import edu.wustl.xipHost.dataAccess.QueryTarget;
 import edu.wustl.xipHost.dataModel.Patient;
 import edu.wustl.xipHost.dataModel.SearchResult;
 import edu.wustl.xipHost.dataModel.Study;
@@ -25,12 +32,64 @@ import gov.nih.nci.cagrid.ncia.client.NCIACoreServiceClient;
  * @author Jaroslaw Krych
  *
  */
-public class GridQuery implements Runnable {
+public class GridQuery implements Runnable, Query {
 	final static Logger logger = Logger.getLogger(GridQuery.class);
 	CQLQuery cql;
 	GridLocation gridLocation;
+	Map<Integer, Object> dicomCriteria;
+	Map<String, Object> aimCriteria;
+	QueryTarget target;
 	SearchResult previousSearchResult;
 	Object queriedObject;
+	GridManager gridMgr = GridManagerFactory.getInstance();
+	GridUtil gridUtil;
+	
+	public GridQuery(GridLocation location){
+		this.gridLocation = location;
+		try {
+			InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("NCIAModelMap.properties");
+			gridUtil = gridMgr.getGridUtil();
+			gridUtil.loadNCIAModelMap(inputStream);
+			System.setProperty("org.apache.commons.logging.Log","org.apache.commons.logging.impl.NoOpLog");		
+		} catch (FileNotFoundException e) {
+			logger.error(e, e);
+		} catch (IOException e) {
+			logger.error(e, e);		}
+	}
+	
+	@Override
+	public void setQuery(Map<Integer, Object> dicomCriteria, Map<String, Object> aimCriteria, QueryTarget target, SearchResult previousSearchResult, Object queriedObject) {
+		this.dicomCriteria = dicomCriteria; 
+		cql = gridUtil.convertToCQLStatement(dicomCriteria, CQLTargetName.PATIENT);
+		this.aimCriteria = aimCriteria; 
+		this.target = target; 
+		this.previousSearchResult = previousSearchResult;
+		this.queriedObject = queriedObject; 
+		if(logger.isDebugEnabled()){
+			String strCQL = "";
+			try {
+				strCQL = ObjectSerializer.toString(cql, new QName("http://CQL.caBIG/1/gov.nih.nci.cagrid.CQLQuery", "CQLQuery"));
+			} catch (SerializationException e) {			
+				logger.error(e, e);
+			}
+			logger.debug("CQL query statement: " + "\r\n" + "\r\n" + strCQL);
+			logger.debug("Grid location: " + gridLocation.toString());
+			if(previousSearchResult == null){
+				logger.debug("Previous search result: " + previousSearchResult);
+			}else{
+				logger.debug("Previous search result: " + previousSearchResult.toString());
+			}
+			if(queriedObject == null){
+				logger.debug("Queried object: " + queriedObject);
+			}else if(queriedObject instanceof Patient){
+				Patient patient = Patient.class.cast(queriedObject);
+				logger.debug("Queried object: " + patient.toString());
+			}else if(queriedObject instanceof Study){
+				Study study = Study.class.cast(queriedObject);
+				logger.debug("Queried object: " + study.toString());
+			}
+		}	
+	}
 	
 	/**
 	 * @param cql - CQL query statement
@@ -75,24 +134,18 @@ public class GridQuery implements Runnable {
 		try {		 
 			searchResult = query(cql, gridLocation, previousSearchResult, queriedObject);
 			logger.info("GRID query finished.");
-			fireUpdateUI();			
+			fireResultsAvailable();			
 		} catch (MalformedURIException e) {
 			logger.error(e, e);
 			searchResult = null;
-			//fireUpdateUI();
-			notifyException(e.getMessage());
 			return;
 		} catch (RemoteException e) {
 			logger.error(e, e);
 			searchResult = null;
-			notifyException(e.getMessage());
-			//fireUpdateUI();
 			return;
 		} catch (ConnectException e) {
 			logger.error(e, e);
 			searchResult = null;
-			//fireUpdateUI();
-			notifyException(e.getMessage());
 			return;
 		}		
 	}
@@ -130,20 +183,20 @@ public class GridQuery implements Runnable {
         return result;			
 	}		
 	
-	
+	@Override
 	public SearchResult getSearchResult(){
 		return searchResult;
 	}
 	
-	GridSearchListener listener;
-    public void addGridSearchListener(GridSearchListener l) {        
-        listener = l;          
-    }
-	void fireUpdateUI(){
-		GridSearchEvent event = new GridSearchEvent(this);         		
-        listener.searchResultAvailable(event);
+	DataAccessListener listener;
+	@Override
+	public void addDataAccessListener(DataAccessListener l) {
+		listener = l;    
+		
 	}
-	void notifyException(String message){         		
-        listener.notifyException(message);
+	
+	void fireResultsAvailable(){
+		QueryEvent event = new QueryEvent(this);         		
+        listener.queryResultsAvailable(event);
 	}
 }
