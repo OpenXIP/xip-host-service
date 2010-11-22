22b9454ca860b2e6dab9313ef6445fd47be6ca0a
Krych, Jaroslaw <jarek.krych@gmail.com>
2010-11-22 22:11:12 +0000

    Added logging statements to Criteria class of the iterator package.
    SVN-Revision: 652

--- src/edu/wustl/xipHost/iterator/Criteria.java
+++ src/edu/wustl/xipHost/iterator/Criteria.java
@@ -3,13 +3,17 @@
  */
 package edu.wustl.xipHost.iterator;
 
+import java.util.Iterator;
 import java.util.Map;
+import org.apache.log4j.Logger;
+import edu.wustl.xipHost.dataAccess.Util;
 
 /**
  * @author Jaroslaw Krych
  *
  */
 public class Criteria {
+	final static Logger logger = Logger.getLogger(Criteria.class);
 	Map<Integer, Object> dicomCriteria;
 	Map<String, Object> aimCriteria;
 	
@@ -29,5 +33,25 @@ public class Criteria {
 	public Map<String, Object> getAIMCriteria(){
 		return aimCriteria;
 	}
-
+	
+	public static void logDicomCriteria(Map<Integer, Object> dicomCriteria){
+		logger.debug("DICOM criteria:");
+		Iterator<Integer> iter = dicomCriteria.keySet().iterator();
+		while(iter.hasNext()){
+			Integer key = iter.next();
+			String keyHexValue = Util.toDicomHex(key);
+			String value = (String)dicomCriteria.get(key);
+			logger.debug(keyHexValue + " " + value);
+		}
+	}
+	
+	public static void logAimCriteria(Map<String, Object> aimCriteria){
+		logger.debug("AIM criteria:");
+		Iterator<String> iter = aimCriteria.keySet().iterator();
+		while(iter.hasNext()){
+			String key = iter.next();
+			String value = (String) aimCriteria.get(key);
+			logger.debug(key + " " + value);
+		}
+	}
 }
